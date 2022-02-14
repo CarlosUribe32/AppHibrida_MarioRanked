277f83a675e381b03686a83e77507869475bfe86
Jean-Pierre-A <jean.agudelo@upb.edu.co>
2022-02-13 21:42:38 -0500

    Funcionalidad Login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,56 @@
+let logVolver;
+let logListo;
+let logTxt;
 function login(){
+
+    logVolver = document.getElementById("log-btnvolver");
+
+    logVolver.addEventListener("click", logNavegacion);
+
+    logListo = document.getElementById("log-btnlisto");
     
+    logListo.addEventListener("click",logLista);
+
+
+
+    
+}
+
+function logNavegacion(){
+
+    location.href="index.html";
+
+}
+
+function logLista (){
+    logTxt = document.getElementById("log-txtnombre").value;
+    console.log(logTxt);
+
+   if(logTxt=="")
+        alert("Porfavor ingrese el nombre, gracias.");
+
+    else {
+
+        console.log(window.localStorage.getItem(logTxt));
+        if(window.localStorage.getItem(logTxt)!=null)
+        {
+            let opcion = confirm("El nombre ya existe, ¿Deseas cambiarlo?");
+            if(opcion == true)
+            {
+                window.localStorage.removeItem(logTxt);
+                window.localStorage.setItem(logTxt,0);
+                location.href = "personajes.html";
+            }
+            
+
+        }
+        else
+        {
+            window.localStorage.setItem(logTxt,0);
+            location.href = "personajes.html";
+
+        }
+
+    }
+
 }
