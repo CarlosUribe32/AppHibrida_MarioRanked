08f0d7995f8c735766bea10f9483dadf22ec67ec
Nicolas <juann.ruiz@upb.edu.co>
2022-02-13 20:39:56 -0500

    Navegacion del index

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+let boton_jugar;
+let boton_creditos;
+let boton_puntuacion;
 function index(){
+    boton_jugar = document.getElementById("index_btn_jugar");
+    boton_creditos = document.getElementById("index_btn_creditos");
+    boton_puntuacion = document.getElementById("index_btn_puntuacion");
     
+    boton_jugar.addEventListener("click",nav_jugar);
+    boton_creditos.addEventListener("click",nav_creditos);
+    boton_puntuacion.addEventListener("click",nav_puntuacion);
+}
+function nav_jugar(){
+    location.href = "login.html";
+}
+function nav_creditos(){
+    location.href = "creditos.html";
+}
+function nav_puntuacion(){
+    location.href = "puntuacion.html";
 }
