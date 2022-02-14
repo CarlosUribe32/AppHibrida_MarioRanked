0359139677c3caca23a284e097a055e4c272c615
Candre <carlos.uribecupb.edu.co>
2022-02-13 20:15:21 -0500

    Personaje seleccionado almacenado en el LS

--- js/personajes.js
+++ js/personajes.js
@@ -1,10 +1,14 @@
 let personajeSeleccionado;
+
 function personajes(){
-    
+    if(localStorage.getItem('personajeSeleccionado')!==null)
+        localStorage.removeItem('personajeSeleccionado');
+    console.log(localStorage.getItem('personajeSeleccionado'));
+
 }
 
 function personajes_seleccion(elPersonaje){
     personajeSeleccionado = elPersonaje;
-    console.log(personajeSeleccionado);
+    localStorage.setItem('personajeSeleccionado', personajeSeleccionado);
     location.href = "niveles.html"
 }
