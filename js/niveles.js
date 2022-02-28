e6678ad9f88ea9b95b66a6f80642da57ab1476a6
Nicolas <juann.ruiz@upb.edu.co>
2022-02-27 21:33:10 -0500

    Conexion nivel 3 con niveles

--- js/niveles.js
+++ js/niveles.js
@@ -1,9 +1,10 @@
 function niveles(){
     let niveles_nivel1 = document.getElementById("niveles_div_nivel1");
     let niveles_nivel2 = document.getElementById("niveles_div_nivel2");
-
+    let niveles_nivel3 = document.getElementById("niveles_div_nivel3");
     niveles_nivel1.addEventListener("click",niveles_aNivel1);
     niveles_nivel2.addEventListener("click",niveles_aNivel2);
+    niveles_nivel3.addEventListener("click",niveles_aNivel3);
 }
 
 function niveles_aNivel1(){
@@ -11,4 +12,7 @@ function niveles_aNivel1(){
 }
 function niveles_aNivel2(){
     location.href="nivel2.html";
+}
+function niveles_aNivel3(){
+    location.href="nivel3.html";
 }
